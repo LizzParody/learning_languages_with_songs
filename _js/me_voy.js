bbd9b827138b541ae4d71ee24fb790ee0ec897df
Lyzzeth Parody Álvarez <lizparody@gmail.com>
2015-11-05 16:35:16 -0500

    finish the filling the blank songsgit status

--- _js/me_voy.js
+++ _js/me_voy.js
@@ -1,74 +1,20 @@
-$(document).ready(function(){
-$('#word1').first().focus(function() {
-    console.log('in');
-}).blur(function() {
-    console.log('out');
-});
-
-$('#word1').first().focus(function() {
-    console.log('in');
-}).blur(function() {
-    console.log('out');
-});
-
-$('#word2').first().focus(function() {
-    console.log('in');
-}).blur(function() {
-    console.log('out');
-});
-
-$('#word3').first().focus(function() {
-    console.log('in');
-}).blur(function() {
-    console.log('out');
-});
-
-$('#word4').first().focus(function() {
-    console.log('in');
-}).blur(function() {
-    console.log('out');
-});
-
-$('#word5').first().focus(function() {
-    console.log('in');
-}).blur(function() {
-    console.log('out');
-});
-
-$('#word6').first().focus(function() {
-    console.log('in');
-}).blur(function() {
-    console.log('out');
-});
-
-$('#word7').first().focus(function() {
-    console.log('in');
-}).blur(function() {
-    console.log('out');
-});
-
-$('#word8').first().focus(function() {
-    console.log('in');
-}).blur(function() {
-    console.log('out');
-});
-
-$('#word9').first().focus(function() {
-    console.log('in');
-}).blur(function() {
-    console.log('out');
-});
-
-$('#word10').first().focus(function() {
-    console.log('in');
-}).blur(function() {
-    console.log('out');
-});
-
-$('#word11').first().focus(function() {
-    console.log('in');
-}).blur(function() {
-    console.log('out');
-});
-
+$(document).ready(function() {
+  function colorize(input) {
+    input.focus(function() {
+      console.log('in');
+    }).blur(function() {
+        var v = $(this).val();
+        var a = $(this).data("answer");
+        if (v === a) {
+          $(this).addClass("correct");
+          $(this).removeClass("incorrect");
+        } else {
+          $(this).addClass("incorrect");
+          $(this).removeClass("correct");
+        }
+    });
+  }
+  $.each($("input"), function( index, value ) {
+    colorize($(value));
+  });
 });
