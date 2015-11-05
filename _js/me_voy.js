07def2201de2b33b8f9ac225d261516c88ab78f7
Lyzzeth Parody Álvarez <lizparody@gmail.com>
2015-11-05 13:18:16 -0500

    add the css, js and html of filling the blank songs

---
+++ _js/me_voy.js
@@ -0,0 +1,74 @@
+$(document).ready(function(){
+$('#word1').first().focus(function() {
+    console.log('in');
+}).blur(function() {
+    console.log('out');
+});
+
+$('#word1').first().focus(function() {
+    console.log('in');
+}).blur(function() {
+    console.log('out');
+});
+
+$('#word2').first().focus(function() {
+    console.log('in');
+}).blur(function() {
+    console.log('out');
+});
+
+$('#word3').first().focus(function() {
+    console.log('in');
+}).blur(function() {
+    console.log('out');
+});
+
+$('#word4').first().focus(function() {
+    console.log('in');
+}).blur(function() {
+    console.log('out');
+});
+
+$('#word5').first().focus(function() {
+    console.log('in');
+}).blur(function() {
+    console.log('out');
+});
+
+$('#word6').first().focus(function() {
+    console.log('in');
+}).blur(function() {
+    console.log('out');
+});
+
+$('#word7').first().focus(function() {
+    console.log('in');
+}).blur(function() {
+    console.log('out');
+});
+
+$('#word8').first().focus(function() {
+    console.log('in');
+}).blur(function() {
+    console.log('out');
+});
+
+$('#word9').first().focus(function() {
+    console.log('in');
+}).blur(function() {
+    console.log('out');
+});
+
+$('#word10').first().focus(function() {
+    console.log('in');
+}).blur(function() {
+    console.log('out');
+});
+
+$('#word11').first().focus(function() {
+    console.log('in');
+}).blur(function() {
+    console.log('out');
+});
+
+});
